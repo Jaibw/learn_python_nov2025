d46f2084adf0b231acfc8bd137a371aa168f355d
Jaibw <jai.unix@gmail.com>
2025-11-07 06:42:28 +0530

    Create hello.py

---
+++ src/hello.py
@@ -0,0 +1,9 @@
+"""
+This is a sample program
+Date: Nov 07, 2025 6:40 AM IST
+Owner: Jai
+"""
+
+print("Hello World")   # I am printing hello world
+print("Hello Jai")     # I am printing hello jai
+# print(1+2)
